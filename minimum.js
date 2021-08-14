2825aef7a5d6cf47b5f1f559138e0250f34158f9
Jônatas <jonatas.eoliveira@gmail.com>
2021-08-14 10:54:13 -0400

    Adding some exercises about functions

---
+++ minimum.js
@@ -0,0 +1,12 @@
+// A program that takes two arguments and returns their minimum
+
+function minimum(numberA, numberB) {
+    return numberA < numberB ? numberA : numberB;
+}
+
+console.log(minimum(1, 2));
+console.log(minimum(3, 1));
+console.log(minimum(2, 2));
+
+
+
